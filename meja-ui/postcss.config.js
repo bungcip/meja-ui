ddf43422d60adb3f2e11c649f68acd97cf4df4bf
Gigih Aji Ibrahim <bungcip@gmail.com>
2025-09-02 08:15:24 +0700

    some build fixes

--- meja-ui/postcss.config.js
+++ meja-ui/postcss.config.js
@@ -1,6 +1,8 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    // Tailwind v4 extracts the PostCSS plugin to a separate package
+    // Use @tailwindcss/postcss here to avoid the runtime error seen in Vite.
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 }
